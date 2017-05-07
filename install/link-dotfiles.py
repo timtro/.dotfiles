494bd32581067b755a34bf0cd379d75fc0ed3d57
timtro <timtro@gmail.com>
2017-05-07 16:41:18 -0400

    Wrote and tested dotfile linker

--- install/link-dotfiles.py
+++ install/link-dotfiles.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 
-# from plumbum import local
 import logging
 import sys
+from plumbum import local
 
 from modules.ScriptHelpers import symlink_with_checks
 
 
 def main():
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    linkables = local['find']["-H", local.cwd.up(1), "-maxdepth", "3", "-name",
+                              "*.symlink"]().split()
+    logging.info("Found linkables: " + "\n".join(linkables))
 
-    # linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
+    list(map(link_a_linkable, linkables))
 
-    for target in linkables:
-        symlink_with_checks(srcPath / target, destPath / target)
+
+def link_a_linkable(tgt):
+    dest = local.env.home / ('.' + local.path(tgt).basename.strip(".symlink"))
+    symlink_with_checks(local.path(tgt), dest)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     main()
