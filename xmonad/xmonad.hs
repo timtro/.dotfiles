c9c26e512369d35e63ed57a9f7f0aab1692c2ecc
Timtro <timtro@gmail.com>
2018-02-09 11:19:00 -0500

    Alright, I'm going to try commas in front for a while.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -21,33 +21,36 @@ myStartupHook = do
   spawn "xautolock -time 7 -locker \"gnome-screensaver-command -l\" -notify 10 -notifier \"notify-send -t 5000 -i gtk-dialog-info \'Locking in 10 seconds\'\""
 
 myConfig = defaultConfig {
-  modMask = mod4Mask, -- Use Super instead of Alt
-  startupHook = myStartupHook,
-  layoutHook = smartBorders $ spacing 5 $ Tall 1 (3/100) (1/2),
-  logHook = fadeInactiveLogHook 0.9,
-  handleEventHook = fullscreenEventHook,
-  terminal = "urxvt",
-  borderWidth = 0,
-  normalBorderColor = "#181715",
-  focusedBorderColor = "#58C5F1",
-  focusFollowsMouse = False
+    modMask            = mod4Mask -- Use Super instead of Alt
+  , startupHook        = myStartupHook
+  , layoutHook         = smartBorders $ spacing 5 $ Tall 1 (3/100) (1/2)
+  , logHook            = fadeInactiveLogHook 0.9
+  , handleEventHook    = fullscreenEventHook
+  , terminal           = "urxvt"
+  , borderWidth        = 0
+  , normalBorderColor  = "#181715"
+  , focusedBorderColor = "#58C5F1"
+  , focusFollowsMouse  = False
 } `additionalKeys` myKeys `removeKeys` [(mod4Mask, xK_q)]
 
 myKeys = [
-  ((mod4Mask .|. shiftMask, xK_r), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"),
-  ((mod4Mask .|. shiftMask, xK_w), spawn "/home/timtro/scr/wallpaper-shuffle.sh"),
-  ((mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command -l")
+    ((mod4Mask .|. shiftMask, xK_r),
+      spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi")
+  , ((mod4Mask .|. shiftMask, xK_w),
+      spawn "/home/timtro/scr/wallpaper-shuffle.sh")
+  , ((mod4Mask .|. shiftMask, xK_l),
+      spawn "gnome-screensaver-command -l")
   ]
 
 myBar = "xmobar /home/timtro/.xmonad/xmobarrc"
 
 myPP = xmobarPP {
-  ppCurrent = xmobarColor "#181715" "#58C5F1" . wrap "[" "]",
-  ppTitle = xmobarColor "#14FF08" "" . shorten 120,
-  ppVisible = xmobarColor "#58C5F1" "#181715" . wrap "(" ")",
-  ppUrgent = xmobarColor "#181715" "#D81816",
-  ppHidden = xmobarColor "#58C5F1" "#181715",
-  ppSep = " * "
+    ppCurrent = xmobarColor "#181715" "#58C5F1" . wrap "[" "]"
+  , ppTitle   = xmobarColor "#14FF08" "" . shorten 120
+  , ppVisible = xmobarColor "#58C5F1" "#181715" . wrap "(" ")"
+  , ppUrgent  = xmobarColor "#181715" "#D81816"
+  , ppHidden  = xmobarColor "#58C5F1" "#181715"
+  , ppSep     = " * "
 }
 
 -- keybinding for toggling the gap for the statusbar
