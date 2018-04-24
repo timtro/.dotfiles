bd6f048daedc66cc26a8b4af3ec75e1adfec095b
timtro <timtro@gmail.com>
2018-04-24 00:40:02 -0400

    Font tests and functions for decorating text.

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -25,3 +25,21 @@ function term_size {
 function svg2png {
   inkscape --export-png=$2 --export-dpi=300 --export-background-opacity=0 --without-gui $1
 }
+
+function term_fonts_test {
+  echo -e "\e[1mbold\e[0m"            
+  echo -e "\e[3mitalic\e[0m"
+  echo -e "\e[1m\e[3mBolditalic\e[0m"
+  echo -e "\e[4munderline\e[0m"
+  echo -e "\e[9mstrikethrough\e[0m"
+  echo -e "\e[31mRed\e[0m"
+  echo -e "\x1B[31mHello World\e[0m"
+}
+
+function ansi { echo -e "\e[${1}m${*:2}\e[0m"; }
+function bold { ansi 1 "$@"; }
+function italic { ansi 3 "$@"; }
+function underline { ansi 4 "$@"; }
+function strikethrough { ansi 9 "$@"; }
+function red { ansi 31 "$@"; }
+
