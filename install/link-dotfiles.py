9c5a06c735fdf7043833b9a6c7841345cdddc981
timtro <timtro@gmail.com>
2018-05-18 13:30:14 -0400

    Now links incfg files.

--- install/link-dotfiles.py
+++ install/link-dotfiles.py
@@ -8,31 +8,46 @@ from modules.ScriptHelpers import symlink_with_checks, UnavoidedCollision, user_
 
 
 def main():
-    linkables = local['find']["-H", local.cwd.up(1), "-maxdepth", "3", "-name",
-                              "*.symlink"]().split()
-    logging.info("Found linkables:\n" + "\n".join(linkables))
 
-    if user_says_yes("Proceed?"):
-
-        linked = list(map(link_a_linkable, linkables))
-
-        for t, d in zip(linkables, linked):
-            logging.info(t + " -> " + d)
-
-        git["clone", "https://github.com/VundleVim/Vundle.vim.git", local.env.
-            home / ".vim/bundle/Vundle.vim"]
-        local['vim']["+PluginInstall", "+qall"]
+    link_dotfiles()
+    link_incfg()
 
     # TODO:
     # * ln -s $HOME/.dotfiles/zsh/themes/timtros-agnoster.zsh-theme $HOME/.oh-my-zsh/themes/.
     # *
 
+def link_dotfiles():
+    linkables = local['find']["-H", local.cwd.up(1), "-maxdepth", "3", "-name",
+                              "*.symlink"]().split()
+    logging.info("Found linkable dotfiles:\n" + "\n".join(linkables))
 
-def link_a_linkable(tgt):
+    if user_says_yes("Proceed?"):
+        linked = list(map(link_a_dotfile, linkables))
+        for t, d in zip(linkables, linked):
+            logging.info(t + " -> " + d)
+
+def link_incfg():
+    linkables = local['find']["-H", local.cwd.up(1), "-maxdepth", "3", "-name",
+                              "*.incfg"]().split()
+    logging.info("Found linkable ~/.config/ directories:\n"
+            + "\n".join(linkables))
+
+    if user_says_yes("Proceed, linking in `~/.config?`"):
+        linked = list(map(link_an_incfg, linkables))
+        for t, d in zip(linkables, linked):
+            logging.info(t + " -> " + d)
+
+
+def link_a_dotfile(tgt):
     dest = local.env.home / (
         '.' + local.path(tgt).basename.replace(".symlink", ""))
     return symlink_with_checks(local.path(tgt), dest)
 
+
+def link_an_incfg(tgt):
+    dest = local.env.home / ".config" / (
+        local.path(tgt).basename.replace(".incfg", ""))
+    return symlink_with_checks(local.path(tgt), dest)
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
