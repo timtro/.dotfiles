8b5a63911cbd83cd19d7e04b8968fd87dbf10ef3
timtro <timtro@gmail.com>
2018-05-18 16:42:29 -0400

    Experimental changes for testing.

--- install/install-packages.py
+++ install/install-packages.py
@@ -14,10 +14,7 @@ from modules.ScriptHelpers import throw_if_nonexistant, user_says_yes, \
 
 def main(pkgs, pkmgr, pymgr):
 
-    if user_says_yes("Configure git for timtro?"):
-        git["config", "--global", "user.name", "timtro"]
-        emailAddy = user_reply('Email address?: ')
-        git["config", "--global", "user.email", emailAddy]
+    scripts = local.cwd / "scr"
 
     if pkgs:
         files = list(map(throw_if_nonexistant, map(local.path, pkgs)))
@@ -48,83 +45,29 @@ def main(pkgs, pkmgr, pymgr):
         if user_says_yes("Would you like to install these packages?"):
             pkmgr.install(pkgs)
 
-    if user_says_yes("Would you like to install Google's Chrome browser?"):
-        install_chrome(pkmgr)
-
     if user_says_yes("Would you like to install atom and stared packages?"):
-        install_atom(pkmgr)
-
-    if user_says_yes("Would you like to clone .dotfiles from github?"):
-        git["-C", "/home/timtro/", "clone", "--verbose",
-            "https://github.com/timtro/.dotfiles"] & FG
+        mgr.ppa_install('ppa:webupd8team/atom', 'atom')
+        local['apm']['stars', '--install'] & FG
 
     if user_says_yes("Would you like to install oh-my-zsh?"):
-        install_oh_my_zsh(pkmgr)
+        sh[scripts / "install_zsh.sh"]
 
     if user_says_yes("Would you like to install gtk3 themes?"):
-        install_themes()
+        git["-C", "/home/timtro/", "clone", "--verbose",
+            "https://github.com/tliron/install-gnome-themes"] & FG
+        local['/home/timtro/install-gnome-themes/install-gnome-themes'] & FG
 
     if user_says_yes("Would you like to install the papirus icons?"):
-        install_papirus(pkmgr)
+        pkmgr.ppa_install("ppa:papirus/papirus", ["papirus-icon-theme"])
 
     if user_says_yes("Would you like to install Variety?"):
-        install_variety(pkmgr)
+        pkmgr.ppa_install("ppa:peterlevi/ppa", ["variety", "variety-slideshow"])
 
     if user_says_yes("Would you like to install Jupyter?"):
         pymgr.install("jupyter")
 
-    if user_says_yes("Would you like to install powerline?"):
-        # Must install from git due to Issue #34:
-        #    https://github.com/powerline/powerline/issues/34
-        # pymgr["install", "--user", "git+git://github.com/Lokaltog/powerline"]
-        pymgr.install("git+git://github.com/Lokaltog/powerline")
-
     if user_says_yes("Would you like to install Boomaga?"):
-        install_boomaga(pkmgr)
-
-
-def install_atom(mgr):
-    mgr.ppa_install('ppa:webupd8team/atom', 'atom')
-    local['apm']['stars', '--install'] & FG
-
-
-def install_chrome(mgr):
-    wget[
-        "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"] & FG
-    sudo[local['dpkg']['-i', 'google-chrome-stable_current_amd64.deb']] & FG
-    local["rm"]["./google-chrome-stable_current_amd64.deb"] & FG
-
-
-def install_oh_my_zsh(mgr):
-    """
-    Currently this throws at the end of the script, when it calls chsh.
-    """
-    mgr.install('zsh')
-    try:
-        (curl[
-            "-fsSL",
-            "https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh"]
-         | sh) & FG
-    except ProcessExecutionError:
-        local['chsh']['-s', '/usr/bin/zsh'] & FG
-
-
-def install_themes():
-    git["-C", "/home/timtro/", "clone", "--verbose",
-        "https://github.com/tliron/install-gnome-themes"] & FG
-    local['/home/timtro/install-gnome-themes/install-gnome-themes'] & FG
-
-
-def install_papirus(mgr):
-    mgr.ppa_install("ppa:papirus/papirus", ["papirus-icon-theme"])
-
-
-def install_variety(mgr):
-    mgr.ppa_install("ppa:peterlevi/ppa", ["variety", "variety-slideshow"])
-
-
-def install_boomaga(mgr):
-    mgr.ppa_install("ppa:boomaga/ppa", ["boomaga"])
+        pkmgr.ppa_install('ppa:boomaga/ppa', 'boomaga')
 
 
 def is_comment(str):
