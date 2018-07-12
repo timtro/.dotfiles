1b3609f9d1f5dc2cc7b2ad37b1926d2a71d73ee9
timtro <timtro@gmail.com>
2018-07-12 10:46:01 -0400

    Added instalation of wal and backends to variety setup.

--- install/scripts/setup_variety.sh
+++ install/scripts/setup_variety.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# Pywal and compatible backends:
+pip3 install pywal --user
+pip3 install haishoku --user
+pip3 install colorz --user
+pip3 install colorthief --user
+
 if [ -f $HOME/.config/variety ]; then
   ln -s $HOME/.dotfiles/variety/* $HOME/.config/variety/scripts/.
 else
