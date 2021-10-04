f82b3fdf7ce026b896553c10b7e6d0cbcc19f04d
timtro <timtro@gmail.com>
2021-10-03 21:31:04 -0400

    Aesthetics + tray icon gap now created by using empty XPM.

---
+++ xmonad/xmonad.symlink/xpm/generate_icons.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Pick icons from [Nerd Font Catalogue](https://www.nerdfonts.com/cheat-sheet).
+
+convert -size 22x22 -gravity center -font "/home/timtro/.fonts/type1/nerd-fonts/Hasklug Nerd Font Complete Mono.otf" -pointsize 28 -background none -fill white label:"" nf_beaker_22.xpm
+
+convert -size 22x22 -gravity center -font "/home/timtro/.fonts/type1/nerd-fonts/Hasklug Nerd Font Complete Mono.otf" -pointsize 36 -background none -fill white label:"" nf_haskell_22.xpm
