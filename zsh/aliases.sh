584f9feae790704898c7aadcdfa6908cf9d12ec2
timtro <timtro@gmail.com>
2021-11-15 11:35:06 -0500

    Reorganized, Vim fold-markers added.

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,27 +1,7 @@
+#!/usr/bin/zsh
+# vim: fdm=marker
 
-# Editing and movement shortcuts
-alias edhist='$EDITOR ~/.zsh_history'
-alias eddot='edit_in ~/.dotfiles'
-alias seeunimath='xdg-open /home/timtro/Documents/Standards/unimath-symbols.pdf & disown'
-alias edbib='edit_in ~/texmf/bibtex/bib ~/texmf/bibtex/bib/library.bib'
-alias edtexmf='edit_in ~/texmf/tex/'
-alias ednvim='edit_in ~/.config/nvim/ ~/.config/nvim/init.lua'
-alias edxmonad='eddot ~/.dotfiles/xmonad/xmonad.symlink/xmonad.hs'
-alias edzsh='eddot ~/.dotfiles/zsh/zshrc.symlink'
-alias edaliases='eddot $HOME/.dotfiles/zsh/aliases.sh'
-alias edfuncs='eddot $HOME/.dotfiles/zsh/functions.sh'
-alias edXdefaults='eddot -c "set filetype=xdefaults" ~/.dotfiles/Xorg/Xdefaults.symlink'
-alias edcompose='eddot ~/.XCompose'
-alias edkitty='eddot ~/.dotfiles/kitty/kitty.incfg/kitty.conf'
-alias thesis='cd ~/Documents/Thesis-PhD'
-alias edthesis='thesis && $EDITOR'
-alias edthesis-tex='thesis && cd tex && $EDITOR'
-alias edthesis-md='thesis && cd md && $EDITOR'
-alias edthesis-reports='thesis && cd reports && $EDITOR'
-alias edepigraphs='edit_in ~/Documents/Writing/Epigraphs ~/Documents/Writing/Epigraphs/Epigraphs.tex'
-alias work='cd ~/workspace'
-
-# Utility aliases
+# GNU and shell tools {{{
 alias ls='ls --color=always'
 alias watch_cpu="watch -n 0 'lscpu | grep MHz'"
 alias lsport="netstat -tulpn"
@@ -31,11 +11,6 @@ alias Clear="clear && print_greeting"
 alias seecompose="$EDITOR /usr/share/X11/locale/en_US.UTF-8/Compose"
 alias seekeys="xev | grep -A2 --line-buffered '^KeyRelease' | sed -n '/keycode /s/^.*keycode \([0-9]*\).* (.*, \(.*\)).*$/\1 \2/p'"
 alias bat="upower -i /org/freedesktop/UPower/devices/battery_BAT0"
-
-# Required for kitty to ssh properly:
-if [ "$TERM" = "xterm-kitty" ]; then
-  alias ssh="kitty +kitten ssh"
-fi
 
 if which exa >/dev/null; then
   unalias ll
@@ -47,3 +22,39 @@ fi
 if which fdfind >/dev/null; then
   alias fd="fdfind"
 fi
+
+# Required for kitty to ssh properly:
+if [ "$TERM" = "xterm-kitty" ]; then
+  alias ssh="kitty +kitten ssh"
+fi
+# }}}
+
+# Editing config files {{{
+alias edhist='$EDITOR ~/.zsh_history'
+alias eddot='edit_in ~/.dotfiles'
+alias ednvim='edit_in ~/.config/nvim/ ~/.config/nvim/init.lua'
+alias edxmonad='eddot ~/.dotfiles/xmonad/xmonad.symlink/xmonad.hs'
+alias edzsh='eddot ~/.dotfiles/zsh/zshrc.symlink'
+alias edaliases='eddot $HOME/.dotfiles/zsh/aliases.sh'
+alias edfuncs='eddot $HOME/.dotfiles/zsh/functions.sh'
+alias edXdefaults='eddot -c "set filetype=xdefaults" ~/.dotfiles/Xorg/Xdefaults.symlink'
+alias edcompose='eddot ~/.XCompose'
+alias edkitty='eddot ~/.dotfiles/kitty/kitty.incfg/kitty.conf'
+# }}}
+
+# General work/writing related {{{
+alias work='cd ~/workspace'
+alias seeunimath='xdg-open /home/timtro/Documents/Standards/unimath-symbols.pdf & disown'
+alias edtexmf='edit_in ~/texmf/tex/'
+alias edbib='edit_in ~/texmf/bibtex/bib ~/texmf/bibtex/bib/library.bib'
+alias edepigraphs='edit_in ~/Documents/Writing/Epigraphs ~/Documents/Writing/Epigraphs/Epigraphs.tex'
+# }}}
+
+# Thesis shortcuts {{{
+alias thesis='cd ~/Documents/Thesis-PhD'
+alias edthesis='thesis && $EDITOR'
+alias edthesis-tex='thesis && cd tex && $EDITOR'
+alias edthesis-md='thesis && cd md && $EDITOR'
+alias edthesis-reports='thesis && cd reports && $EDITOR'
+# }}}
+
