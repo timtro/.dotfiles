1c5011895e5c9635eb95efa6694d263d32e206f0
timtro <timtro@gmail.com>
2021-11-22 15:41:06 -0500

    Comment line for evince as a fallback pdf viewer.

--- neovim/nvim.incfg/after/plugin/vimtex.lua
+++ neovim/nvim.incfg/after/plugin/vimtex.lua
@@ -1,4 +1,5 @@
 vim.g.vimtex_view_method = 'mupdf'
 -- vim.g.vimtex_view_method = 'zathura'
+-- vim.g.vimtex_view_general_viewer = 'evince'
 vim.g.vimtex_compiler_latexmk = { continuous = 0 }
 -- vim.g.vimtex_compiler_progname = 'nvr'
