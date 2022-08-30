2d13b594d12d8d940842b5d241c951efe9492a60
timtro <timtro@gmail.com>
2022-08-30 10:37:04 -0400

    Clearer names.

--- neovim/nvim.incfg/lua/tim/functions.lua
+++ neovim/nvim.incfg/lua/tim/functions.lua
@@ -1,12 +1,12 @@
 local M = {}
 
-M.cl_regs = function(str)
+M.clear_all_registers = function(str)
   for c in str:gmatch '.' do
     vim.fn.setreg(c, '')
   end
 end
 
-M.cl_alpha_regs = function()
+M.clear_alpha_registers = function()
   local lowercase_alphas = 'abcdefghijklmnopqrstuvwxyz'
   M.cl_regs(lowercase_alphas)
 end
