43bd17db35912c9d9ebd2b7406215a1557dbf100
timtro <timtro@gmail.com>
2022-08-30 10:38:43 -0400

    Borrowed some utils from TJ.

--- neovim/nvim.incfg/lua/tim/globals.lua
+++ neovim/nvim.incfg/lua/tim/globals.lua
@@ -1,13 +1,31 @@
-P = function(v)
+local plenary_loaded, plenary_reload = pcall(require, "plenary.reload")
+local reloader
+
+if plenary_loaded then
+  reloader = plenary_reload.reload_module
+else
+  reloader = require
+end
+
+PT = function(v)
   print(vim.inspect(v))
   return v
+end
+
+RELOAD = function(...)
+  return reloader(...)
+end
+
+RL = function(name)
+  RELOAD(name)
+  return require(name)
 end
 
 DN = function(v, cm)
   local time = os.date '%H:%M'
   local context_msg = cm or ' '
   local msg = context_msg .. ' ' .. time
-  require 'notify'(vim.inspect(v), 'debug', {
+  require 'notify' (vim.inspect(v), 'DEBUG', {
     title = { 'Debug Output', msg },
   })
   return v
