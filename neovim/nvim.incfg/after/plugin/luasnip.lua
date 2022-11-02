f1c608bd6a831d4f58a74c8d65d15149cf2da5de
timtro <timtro@gmail.com>
2022-11-02 10:06:56 -0400

    Resolved mapping conflict with cmp. C-h and C-k for jumping in snips.

--- neovim/nvim.incfg/after/plugin/luasnip.lua
+++ neovim/nvim.incfg/after/plugin/luasnip.lua
@@ -37,20 +37,20 @@ ls.setup {
 }
 
 -- this will expand the current item or jump to the next.
-vim.keymap.set({ "i", "s" }, "<c-k>", function()
+vim.keymap.set({ "i", "s" }, "<C-l>", function()
   if ls.expand_or_jumpable() then
     ls.expand_or_jump()
   end
 end, { silent = true })
 
 -- this moves to the previous item within the snippet
-vim.keymap.set({ "i", "s" }, "<c-j>", function()
+vim.keymap.set({ "i", "s" }, "<C-h>", function()
   if ls.jumpable(-1) then
     ls.jump(-1)
   end
 end, { silent = true })
 
-vim.keymap.set("i", "<c-l>", function()
+vim.keymap.set("i", "<C-<tab>>", function()
   if ls.choice_active() then
     ls.change_choice(1)
   end
