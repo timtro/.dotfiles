19b779572d8948ba5a00f231d26f1edd39366bc7
timtro <timtro@gmail.com>
2022-11-07 21:02:57 -0500

    Removed commented code, added default-install.

--- neovim/nvim.incfg/lua/config/treesitter.lua
+++ neovim/nvim.incfg/lua/config/treesitter.lua
@@ -1,15 +1,15 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = {"lua", "python", "cpp", "latex", "haskell"},
+  ensure_installed = { 'lua', 'python', 'cpp', 'latex', 'haskell', 'markdown' },
   sync_install = false,
   auto_install = true,
   highlight = {
     enable = true,
     disable = function(lang, buf)
-        local max_filesize = 10000 * 1024 -- 10 MB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
+      local max_filesize = 5000 * 1024 -- 5 MB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
     end,
     additional_vim_regex_highlighting = false,
   },
@@ -17,14 +17,6 @@ require('nvim-treesitter.configs').setup {
     enable = true,
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {
-    --   '#FFFFFF',
-    --   '#666666',
-    --   '#FF0000',
-    --   '#00FF00',
-    --   '#0000FF',
-    -- }
-    -- termcolors = {} -- table of colour name strings
   },
   additional_vim_regex_highlighting = false,
   matchup = {
