ad674174a8e73b2d9ee2b98dd5f459a24fd51b28
timtro <timtro@gmail.com>
2022-11-14 10:30:16 -0500

    Notify interface change.

--- neovim/nvim.incfg/lua/util/debug.lua
+++ neovim/nvim.incfg/lua/util/debug.lua
@@ -1,10 +1,14 @@
 local M = {}
 
 function M.alert(v, cm)
-  local time = os.date "%H:%M"
-  local context_msg = cm or " "
-  local msg = context_msg .. " " .. time
-  require "notify"(vim.inspect(v), "debug", { title = { "Debug Output", msg } })
+  local time = os.date '%H:%M'
+  local context_msg = cm or ' '
+  local msg = context_msg .. ' ' .. time
+  require('notify').notify(
+    vim.inspect(v),
+    'debug',
+    { title = { 'Debug Output', msg } }
+  )
   return v
 end
 
