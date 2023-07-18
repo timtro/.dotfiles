022bd51e83b5b026370ed54e657fba45ad756b0a
timtro <timtro@gmail.com>
2023-07-17 21:23:56 -0400

    PPLatex.

--- neovim/nvim.incfg/lua/config/vimtex.lua
+++ neovim/nvim.incfg/lua/config/vimtex.lua
@@ -18,13 +18,25 @@ vim.g.vimtex_fold_manual = 1
 vim.g.latex_indent_enabled = 0
 vim.g.vimtex_complete_enabled = 1
 -- vim.g.vimtex_quickfix_enabled = 0
+vim.g.vimtex_quickfix_method = 'pplatex'
+-- vim.g.vimtex_quickfix_autojump = 1
+-- vim.g.vimtex_quickfix_mode = 1
+vim.g.vimtex_quickfix_open_on_warning = 0
+
 -- vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
 
-vim.cmd[[
+vim.cmd [[
 let g:vimtex_complete_ref = {
   \ 'custom_patterns': ['\\[DNBXFA]ref\*\?{[^}]*$']
   \ }
 ]]
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<Leader>ls',
+  ':VimtexStatus<CR>',
+  { silent = true }
+)
 
 -- For tikzexternalize, to expand memory, need -shell-escape.
 --   https://github.com/lervag/vimtex/issues/467#issuecomment-395840260
